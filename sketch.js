90bc987bf5ef3d2e969ab9e0a6ca4af482df98fb
chiteh <chiteh.chen@gmail.com>
2018-02-22 19:48:46 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+
+function setup() {
+	createCanvas(500,500);
+	fill(150);
+  noStroke();
+ 
+}
+
+function draw() {
+  background(0);
+  fill(0,0,255);
+  ellipse(width/2,height/2,200,200);
+  
+}
